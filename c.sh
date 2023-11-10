65dddc560d027c0cc61ec0eebc6ed1768b458bf6
HermitC <150248673+HermitC@users.noreply.github.com>
2023-11-10 08:13:53 +0100

    Delete c.sh

--- c.sh
+++
@@ -1,3 +0,0 @@
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-sudo nix-channel --update
