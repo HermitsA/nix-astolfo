bbfa057aa9ea81283037079911d0f157c0701820
HermitsA <149957167+HermitsA@users.noreply.github.com>
2023-11-11 15:42:17 +0000

    Add files via upload

---
+++ HM.sh
@@ -0,0 +1,5 @@
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable
+sudo nix-channel --update
+
+
