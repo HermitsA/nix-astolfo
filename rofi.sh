e58d63d11593e0946a03d1cc651a7ccd3e91d2db
HermitsA <149957167+HermitsA@users.noreply.github.com>
2023-11-11 16:38:56 +0000

    Create rofi.sh

---
+++ rofi.sh
@@ -0,0 +1,10 @@
+cp /home/astolfo/nix-astolfo/dotfiles/rofi/* /home/astolfo/.config/rofi
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable
+sudo nix-channel --update
+
+
+
+
+
+
