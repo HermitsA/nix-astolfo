5b8dc3485dcc50e6b5d2daeb08dbb2219360ee81
HermitC <150248673+HermitC@users.noreply.github.com>
2023-11-22 10:51:32 +0100

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,7 +4,7 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
   };
 
-  outputs = { nixpkgs, nix-flatpak, ... }: {
+  outputs = { nix-flatpak, ... }: {
     nixosConfigurations.rider = nixpkgs.lib.nixosSystem {
       modules = [
         nix-flatpak.nixosModules.nix-flatpak
