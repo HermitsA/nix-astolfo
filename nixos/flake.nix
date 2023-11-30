d0f45cec6d0d584a13f8080e3000fe8bf77b460c
HermitsA <149957167+HermitsA@users.noreply.github.com>
2023-11-30 10:39:36 +0100

    Update flake.nix
    flatpak support pls

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,16 +1,31 @@
 {
+  description = "NixOS configuration";
+
   inputs = {
-    # ...
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nix-flatpak, ... }: {
-    nixosConfigurations.rider = nixpkgs.lib.nixosSystem {
-      modules = [
-        nix-flatpak.nixosModules.nix-flatpak
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+	nix-flatpak.nixosModules.nix-flatpak
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jdoe = import ./home.nix;
 
-        ./configuration.nix
-      ];
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
     };
   };
 }
