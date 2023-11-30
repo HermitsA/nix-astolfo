a1aaf2161c2fa81a4d59ab0e343171a7a247634a
HermitsA <149957167+HermitsA@users.noreply.github.com>
2023-11-30 10:55:11 +0100

    Update flake.nix
    usr stuff in configuration.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -17,9 +17,9 @@
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.jdoe = import ./home.nix;
+        #    home-manager.useGlobalPkgs = true;
+        #    home-manager.useUserPackages = true;
+        #    home-manager.users.jdoe = import ./home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
