62fd0743a0cf0c760fbf268fc647416de237247e
Wandering Hermit <149957167+HermitsA@users.noreply.github.com>
2024-07-17 18:44:49 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ sudo nix-channel --add https://github.com/nix-community/home-manager/archive/mas
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable:
 sudo nix-channel --update:
 nixos-install:
-cp nixos-rofi/* /mnt/etc/nixos/:
+
 
 echo type reboot if you want to go into the normal thing.
 
